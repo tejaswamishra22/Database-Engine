8abdbee2d6af806c1c78d386b46ec9b8f67d5b15
Tejaswa Mishra <tejaswamishra22@gmail.com>
2026-07-18 18:39:45 +0000

    Started Database Engine

---
+++ storage/page.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <cstdint>
+
+constexpr std::size_t PAGE_SIZE = 4096;
+
+class Page {
+public:
+    Page() = default;
+
+    std::byte* data(){
+        return buffer.data();
+    }
+
+private:
+    std::array<std::byte, PAGE_SIZE> buffer{};
+};
