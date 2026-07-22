264073b6c98278399a1617f4c13bf1b1cd079197
Tejaswa Mishra <tejaswamishra22@gmail.com>
2026-07-22 16:52:17 +0000

    Open Api implementation

--- storage/disk_manager.cpp
+++ storage/disk_manager.cpp
@@ -5,16 +5,29 @@
 #include <cerrno>
 #include <system_error>
 #include <cstring>
+#include <sys/stat.h>
 
 DiskManager::DiskManager(const std::string& dbName) : fd(-1), name(dbName) {}
 
 void DiskManager::Open() {
     if (fd < 0) {
-        fd = open(name.c_str(), O_RDWR | O_CREAT, 00700);
+        fd = open(name.c_str(), O_RDWR | O_CREAT, 0600);
         if (fd < 0) {
             throw std::system_error(errno, std::generic_category(), "Failed to open " + name);
         }
+        struct stat file_Info;
+        int file_stats = fstat(fd, &file_Info);
+        if(file_stats == 0 && static_cast<int>(file_Info.st_size) > 0){
+            return ;
+        }
+        else if(file_stats < 0){
+            throw std::system_error(errno, std::generic_category(), "Failed to get File Stats " + name);
+        }
+        Page0Header page0;
+        page0.magic = kMagicNumber;
+        page0.version = 1;
+        page0.pageSize = 4096;
         Page pg0;
-        std::memcpy(pg0.getByte(), &kMagicNumber, sizeof(kMagicNumber));
+        std::memcpy(pg0.getByte(), &page0, sizeof(page0));
     }
 }
--- storage/disk_manager.h
+++ storage/disk_manager.h
@@ -4,6 +4,13 @@
 #include <string>
 #include "page.h"
 
+struct Page0Header {
+    uint32_t magic;
+    uint32_t version;
+    uint32_t pageSize;
+};
+
+
 class DiskManager {
 public:
     explicit DiskManager(const std::string& dbName = "database.db");
